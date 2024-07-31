869b9da781e13950264045c7a3f70a33dd8bd88d
muzykalniy <andreimuzyka18@gmail.com>
2024-07-31 13:49:59 +0200

    H3 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const a = prompt('Enter first number:');
+const b = prompt('Enter second number:');
+
+
+let num1= Number(a);
+let num2= Number(b);
+
+
+let addition = num1 + num2;
+console.log(`Answer: ${num1} + ${num2} = ${addition}`);
+
+
+let subtraction = num1 - num2;
+console.log(`Answer: ${num1} - ${num2} = ${subtraction}`);
+
+let multiplication= num1 * num2;
+console.log(`Answer: ${num1} * ${num2} = ${multiplication}`);
+
+let division= num1 / num2;
+console.log(`Answer: ${num1} / ${num2} = ${division}`);
+
+
+
