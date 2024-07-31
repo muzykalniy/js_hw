2ebd439f1d9767e4253b29158c9605487fdd4bec
muzykalniy <andreimuzyka18@gmail.com>
2024-07-31 13:10:03 +0200

    H2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Task #1
+
+let userName = 'John';
+let adminName = userName;
+
+alert(`admin: ${adminName}`);
+
+
+// Task #2
+
+let planetName = 'Earth';
+let userLogin = 'John';
+
+
+// Task #3
+
+let str = "10";
+let num = 10;
+
+let sum = str + num;
+
+console.log(typeof sum);
+
+
+
+
