c478f069ade27eba2938d5a84ea038ae5f890c23
muzykalniy <andreimuzyka18@gmail.com>
2024-08-01 12:52:40 +0200

    H3 fix completed

--- js/script.js
+++ js/script.js
@@ -4,21 +4,21 @@ const a = prompt('Enter first number:');
 const b = prompt('Enter second number:');
 
 
-let num1= Number(a);
-let num2= Number(b);
+const num1= Number(a);
+const num2= Number(b);
 
 
-let addition = num1 + num2;
+const addition = num1 + num2;
 console.log(`Answer: ${num1} + ${num2} = ${addition}`);
 
 
-let subtraction = num1 - num2;
+const subtraction = num1 - num2;
 console.log(`Answer: ${num1} - ${num2} = ${subtraction}`);
 
-let multiplication= num1 * num2;
+const multiplication= num1 * num2;
 console.log(`Answer: ${num1} * ${num2} = ${multiplication}`);
 
-let division= num1 / num2;
+const division= num1 / num2;
 console.log(`Answer: ${num1} / ${num2} = ${division}`);
 
 
