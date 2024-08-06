7dd3c32e3e87d020ca6ebf56a71d3d7e88608210
muzykalniy <andreimuzyka18@gmail.com>
2024-08-06 21:18:57 +0200

    H8 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const firstNum = +prompt('Please enter a first number');
+const secondNum = +prompt('Please enter a second number');
+const thirdNum = +prompt('Please enter a third number');
+
+// const roundedAverageNumber = averageNumber.toFixed(2);
+// alert(`Average number is: ${roundedAverageNumber}`);
+
+
+const averageNumber = (firstNum + secondNum + thirdNum) / 3;
+// const roundedAverageNumber = averageNumber.toFixed(2);
+
+alert(`Average number is: ${averageNumber}`);
+// alert(`Average number is: ${roundedAverageNumber}`);
