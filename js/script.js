b49d92d546f491f91c97fde75c24dc7126db04fe
muzykalniy <andreimuzyka18@gmail.com>
2024-08-06 21:43:13 +0200

    H7 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const hoursNumber = +prompt('Please enter an hours number');
+
+
+const minutesNumber = hoursNumber * 60;
+const secondsNumber = hoursNumber * 3600;
+
+
+alert(`${hoursNumber} hours consist of ${minutesNumber} minutes which are ${secondsNumber} seconds`);
+
+
+
+
