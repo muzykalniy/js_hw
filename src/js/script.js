d446fe5cf9b9a0b55051562935cb77beea172905
muzykalniy <andreimuzyka18@gmail.com>
2024-08-28 12:00:24 +0200

    HW 15 correction w/o comments

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,6 @@ const numberInput = +prompt('Enter random number of symbols:');
 const generateKey = function (length, characters) {
   let result = '';
 
-
   for (let i = 0; i < length; i++) {
     const randomIndex = Math.floor(Math.random() * characters.length);
     result += characters[randomIndex];
