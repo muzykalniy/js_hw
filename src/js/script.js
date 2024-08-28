71c715158f5e59919316ee019ad0ea5bfce0e3b5
muzykalniy <andreimuzyka18@gmail.com>
2024-08-28 14:55:45 +0200

    HW 17 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+const removeElement = function (arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
+};
+console.log(removeElement(array, 5));
