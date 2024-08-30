cdf5f378e2fc701181bf87fa0430094e00b4e332
muzykalniy <andreimuzyka18@gmail.com>
2024-08-30 10:28:23 +0200

    HW 18  done

--- src/js/script.js
+++ src/js/script.js
@@ -51,3 +51,50 @@ console.log(`Result: ${math}`);
 
 // Task 3
 
+// parseInt превращает 3 в настоящее число в 10тичной системе счисления
+const arrayRows = parseInt(prompt('Enter number of rows:'), 10);
+const arrayCols = parseInt(prompt('Enter number of columns:'), 10);
+// создал основной массив
+const arrayMain = [];
+// Функция для заполнения массива данными от пользователя
+const arrayFunc = function (rows, cols) {
+  // проверка
+  if (isNaN(rows) || rows <= 0 || isNaN(cols) || cols <= 0) {
+    alert('Invalid input. Please enter positive numbers for rows and columns.');
+    return;
+  }
+  // заполняю основной массив
+  for (let i = 0; i < rows; i++) {
+    arrayMain[i] = []; // создал внутренний массив для текущей строки и начинает цикл его заполнять
+    // внутренний цикл для заполнения колонок
+    for (let j = 0; j < cols; j++) {
+      // Запрашиваем значение для каждого элемента массива
+      const value = prompt(`Enter value for element [${i}][${j}]:`);
+      arrayMain[i][j] = value; // Сохраняем значение в основной массив
+    }
+  }
+};
+arrayFunc(arrayRows, arrayCols);
+console.log(arrayMain);
+
+
+// Task 4
+
+const stringInput = prompt('Enter string:');
+const symbolInput = prompt('Enter symbols:');
+let newStr = '';
+
+const deleteSymbol = function (str, arr = ['sym1', 'sym2']) {
+  for (let i = 0; i < str.length; i++) {
+    /* includes - метод для проверки содержится ли символ строки в массиве символов на удаление
+     если текущий символ str[i] не содержится в массиве arr, добавляю его в новую строку newStr
+     если содержится - то вернет false и цикл пропустить этот символ */
+    if (!arr.includes(str[i])) {
+      newStr += str[i];
+    }
+  }
+  return newStr;
+};
+// symbolInput.split('') преобразует строку символов, которые нужно удалить, в массив символов.
+const func = deleteSymbol(stringInput, symbolInput.split(''));
+console.log(func);
