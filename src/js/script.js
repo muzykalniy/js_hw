c27a7bd4c80a5b812dfe6719f3aa5f6ca1c0b08c
muzykalniy <andreimuzyka18@gmail.com>
2024-09-10 13:27:27 +0200

    HW 22 corrections

--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,15 @@
 const array = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
 
 // 1
-const positiveItem = array.filter((item) => item > 0);
+const res = array.reduce((acc, item) => {
+  if (item < 0) return acc;
+  acc.sum += item;
+  acc.qty++;
+  return acc;
+}, { sum: 0, qty: 0 });
+console.log(res);
+
+/* const positiveItem = array.filter((item) => item > 0);
 console.log('Positive elements', positiveItem);
 
 const sumPositiveNumFunc = (arr) => {
@@ -19,7 +27,7 @@ const result1 = sumPositiveNumFunc(positiveItem);
 // sum аккумулятора, 0 это начальное значение аккумулятора
 console.log('Sum of the positive elements', result1);
 
-console.log('Number of positive elements', positiveItem.length);
+console.log('Number of positive elements', positiveItem.length); */
 
 // 2
 const minNum = array.reduce((min, item) => (item < min ? item : min), array[0]);
