f7817b8bcfe70ffd0377ed65ef2838b68c8f9f05
Andrey Muzyka <andreimuzyka18@gmail.com>
2024-09-19 14:11:17 +0200

    hw 27 done

--- Readme.md
+++ Readme.md
@@ -1,1 +1,25 @@
 ## Опис Домашки
+Написати свою реалізацію функції bind
+
+При реалізації фунції bind заборонено використовувати методи call та apply
+
+const people = [
+{
+firstName: "John",
+lastName: "Doe",
+age: 30
+},
+{
+firstName: "Jane",
+lastName: "Smith",
+age: 25
+},
+{
+firstName: "Alice",
+lastName: "Johnson",
+age: 28
+}
+];
+const  greet = (greeting) => {
+console.log(greeting + ", " + this.firstName);
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const people = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+  },
+  {
+    firstName: 'Jane',
+    lastName: 'Smith',
+    age: 25,
+  },
+  {
+    firstName: 'Alice',
+    lastName: 'Johnson',
+    age: 28,
+  },
+];
+const greet = function (greeting) {
+  console.log(`${greeting}, ${this.firstName}`);
+};
+
+for (const person of people) {
+  // this указывает на объект, привязанный через bind. Поэтому this.firstName будет ссылаться на свойство firstName этого объекта person.
+  // если не передавать person.firstName как аргумент в функции greet, все равно он будет доступен через this.firstName
+  const funcGreet = greet.bind(person, 'Hello');
+  funcGreet();
+}
