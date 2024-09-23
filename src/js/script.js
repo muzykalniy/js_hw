90b767649c1343ad4e8a2d10e13487ccd6b0d296
Andrey Muzyka <andreimuzyka18@gmail.com>
2024-09-23 09:50:45 +0200

    hw 30 done

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const sumFunc = () => {
+  let sumNum = 0;
+  const sumFuncInner = (numInner) => {
+    sumNum += numInner;
+    return sumNum;
+  };
+  return sumFuncInner;
+};
+
+const sum = sumFunc();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
+
