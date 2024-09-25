144386477ce1d09e5dce2a8db25e0697353f7dc2
Andrey Muzyka <andreimuzyka18@gmail.com>
2024-09-25 11:06:19 +0200

    hw 27 corrected

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,98 @@
 'use strict';
 
-const people = [
-  {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 30,
-  },
-  {
-    firstName: 'Jane',
-    lastName: 'Smith',
-    age: 25,
-  },
-  {
-    firstName: 'Alice',
-    lastName: 'Johnson',
-    age: 28,
-  },
-];
-const greet = function (greeting) {
-  console.log(`${greeting}, ${this.firstName}`);
+const guitar = {
+  guitarBrand: 'Gibson',
+  guitarType: 'Les Paul Standard 50s',
+};
+// функция ничего не делает, просто возвращает
+const getGuitarName = function () {
+  return `${this.guitarBrand} ${this.guitarType}`;
 };
 
-for (const person1 of people) {
-  // this указывает на объект, привязанный через bind. Поэтому this.firstName будет ссылаться на свойство firstName этого объекта person.
-  // если не передавать person.firstName как аргумент в функции greet, все равно он будет доступен через this.firstName
-  const funcGreet = greet.bind(person1, 'Hello');
-  funcGreet();
-}
-
-
-
-// решил ниже потренироваться, пока в процессе решения
-
-// 1
-const greetAndAge = function (greeting2) {
-  console.log(`${greeting2}, ${this.firstName}. You are ${this.age} years old`);
+const bind = (func = null, context = undefined, ...args) => {
+  if (!func) return undefined;
+  // bind возвращает новую функцию
+  return function () {
+    return func.apply(context, ...args);
+  };
 };
 
-for (const person2 of people) {
-  const funcGreet2 = greetAndAge.bind(person2, 'Hello');
-  funcGreet2();
-}
+// байндим контекст функции getGuitarName, на ее базе получить новую функцию
+const bindedGuitarName = bind(getGuitarName, guitar);
+console.log(bindedGuitarName);
+console.log(bindedGuitarName());
 
-// 2
 
-const surnameChange = function (newSurname) {
-  this.lastName = newSurname;
-  console.log(`${this.firstName}, your new surname is ${newSurname}`);
-};
-for (const person3 of people) {
-  const newSurnameInput = prompt('Please write new surname:');
-  const funcNewSurname = surnameChange.bind(person3, `${newSurnameInput}`);
-  funcNewSurname();
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const people = [
+//   {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//   },
+//   {
+//     firstName: 'Jane',
+//     lastName: 'Smith',
+//     age: 25,
+//   },
+//   {
+//     firstName: 'Alice',
+//     lastName: 'Johnson',
+//     age: 28,
+//   },
+// ];
+// const greet = function (greeting) {
+//   console.log(`${greeting}, ${this.firstName}`);
+// };
+//
+// for (const person1 of people) {
+//   // this указывает на объект, привязанный через bind. Поэтому this.firstName будет ссылаться на свойство firstName этого объекта person.
+//   // если не передавать person.firstName как аргумент в функции greet, все равно он будет доступен через this.firstName
+//   const funcGreet = greet.bind(person1, 'Hello');
+//   funcGreet();
+// }
+//
+//
+//
+// // решил ниже потренироваться, пока в процессе решения
+//
+// // 1
+// const greetAndAge = function (greeting2) {
+//   console.log(`${greeting2}, ${this.firstName}. You are ${this.age} years old`);
+// };
+//
+// for (const person2 of people) {
+//   const funcGreet2 = greetAndAge.bind(person2, 'Hello');
+//   funcGreet2();
+// }
+//
+// // 2
+//
+// const surnameChange = function (newSurname) {
+//   this.lastName = newSurname;
+//   console.log(`${this.firstName}, your new surname is ${newSurname}`);
+// };
+// for (const person3 of people) {
+//   const newSurnameInput = prompt('Please write new surname:');
+//   const funcNewSurname = surnameChange.bind(person3, `${newSurnameInput}`);
+//   funcNewSurname();
+// }
