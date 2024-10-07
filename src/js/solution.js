14ab6c31f53b9cc24a5031350d610b41997293e1
Andrey Muzyka <andreimuzyka18@gmail.com>
2024-10-07 11:56:09 +0200

    hw 35 till 4

--- src/js/solution.js
+++ src/js/solution.js
@@ -1,1 +1,52 @@
 'use strict';
+
+// 1
+const showEl = (listEl) => {
+  const elementToChoose = document.querySelectorAll(listEl);
+
+  elementToChoose.forEach((el) => {
+    console.log(el, el.textContent);
+  });
+};
+
+showEl('li');
+
+// 2 , 3
+
+// function to add atts' values into the array
+const pushAttValue = (heapOfAtt, arrEmpty) => {
+  for (const attribute of heapOfAtt) {
+    arrEmpty.push(attribute.value);
+  }
+};
+
+// function to add atts' names into the array
+const pushAttName = (heapOfAtt, arrEmpty) => {
+  for (const attribute of heapOfAtt) {
+    arrEmpty.push(attribute.name);
+  }
+};
+
+const getEl = (tag) => {
+  const elements = document.querySelectorAll(tag);
+  elements.forEach((element) => {
+    // empty array for attributes' values
+    const arrAttValues = [];
+    // empty array for attributes' names
+    const arrAttNames = [];
+
+    // getting all attributes from elements
+    const { attributes } = element;
+
+    // call the function to add attributes' values into array
+    pushAttValue(attributes, arrAttValues);
+    // call the function to add attributes' names into array
+    pushAttName(attributes, arrAttNames);
+    console.log(arrAttValues);
+    console.log(arrAttNames);
+  });
+};
+getEl('ul');
+
+// 4
+
